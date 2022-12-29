4ad4bc9494049c9285adcabb4ae82a718af0c3cf
Bruno Dias <brunoadias1996@gmail.com>
2022-12-29 16:11:04 -0300

    many to many with join table

--- src/main/java/com/cursospringinitializr/curso/entities/Product.java
+++ src/main/java/com/cursospringinitializr/curso/entities/Product.java
@@ -5,16 +5,20 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_product")
-public class Product implements Serializable{
+public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
@@ -24,10 +28,15 @@ public class Product implements Serializable{
 	private String description;
 	private Double price;
 	private String imgUrl;
-	
-	@Transient
+
+	@JsonIgnore
+	@ManyToMany
+	@JoinTable(
+			name = "tb_product_category", 
+			joinColumns = @JoinColumn(name = "product_id"), 
+			inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
-	
+
 	public Product() {
 	}
 
@@ -100,6 +109,5 @@ public class Product implements Serializable{
 		Product other = (Product) obj;
 		return Objects.equals(id, other.id);
 	}
-
 
 }
