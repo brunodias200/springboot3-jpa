9e88addcdb78e53e17c8467ba076f8ce5f4e075b
Bruno Dias <brunoadias1996@gmail.com>
2022-12-29 10:51:50 -0300

    service layer component registration

--- src/main/java/com/cursospringinitializr/curso/repositories/UserRepository.java
+++ src/main/java/com/cursospringinitializr/curso/repositories/UserRepository.java
@@ -1,8 +1,8 @@
 package com.cursospringinitializr.curso.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.cursospringinitializr.curso.entities.User;
 
+import com.cursospringinitializr.curso.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
