5eff58a2c28d388bfe5fee790974bf59c520b572
Bruno Dias <brunoadias1996@gmail.com>
2022-12-30 09:34:24 -0300

    Payment, one-to-one association

--- src/main/java/com/cursospringinitializr/curso/config/TestConfig.java
+++ src/main/java/com/cursospringinitializr/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.cursospringinitializr.curso.entities.Category;
 import com.cursospringinitializr.curso.entities.Order;
 import com.cursospringinitializr.curso.entities.OrderItem;
+import com.cursospringinitializr.curso.entities.Payment;
 import com.cursospringinitializr.curso.entities.Product;
 import com.cursospringinitializr.curso.entities.User;
 import com.cursospringinitializr.curso.entities.enums.OrderStatus;
@@ -78,6 +79,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
+		
 		
 	}
 }
--- src/main/java/com/cursospringinitializr/curso/entities/Product.java
+++ src/main/java/com/cursospringinitializr/curso/entities/Product.java
@@ -2,7 +2,6 @@ package com.cursospringinitializr.curso.entities;
 
 import java.io.Serializable;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
